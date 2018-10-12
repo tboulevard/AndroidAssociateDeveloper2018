a6af80609d0b826bf8407530af270db809feb530
tstreetcs <tstreet@vt.edu>
2018-10-12 00:02:31 -0700

    add job scheduler

--- AndroidAssociateDeveloper2018Java/app/src/main/java/com/boulevard/androidassociatedeveloper2018java/JobSchedulerActivity.java
+++ AndroidAssociateDeveloper2018Java/app/src/main/java/com/boulevard/androidassociatedeveloper2018java/JobSchedulerActivity.java
@@ -1,6 +1,12 @@
 package com.boulevard.androidassociatedeveloper2018java;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,16 +14,33 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.boulevard.androidassociatedeveloper2018java.util.JobSchedulerUtil;
 import com.boulevard.androidassociatedeveloper2018java.util.PreferenceUtil;
 
+/**
+ * General activity lifecycle notes:
+ *
+ * Sequence of lifecycle events after device rotation:
+ * onPause, onStop, onDestroy, onCreate, onStart, onResume
+ *
+ */
 public class JobSchedulerActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    /* View holder member variables */
     ImageButton scheduleIncrementCounterButton;
     TextView counterTextView;
     Toolbar mainToolbar;
+    ImageView chargingImageView;
+
+    IntentFilter chargingIntentFilter;
+    /* Important note: If the BroadbastReceiver was defined in the AndroidManifest (statically), registered
+       Intents would trigger it whether the app is running or not. By registering the BroadcastReceiver
+       in onResume and unregistering it in onPause (dynamically), we only allow the broadcast receiver
+       to trigger when the app is open. */
+    ChargingBroadcastReceiver chargingBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +51,7 @@ public class JobSchedulerActivity extends AppCompatActivity implements SharedPre
         scheduleIncrementCounterButton = findViewById(R.id.schedule_increment_counter_button);
         counterTextView = findViewById(R.id.counter_textview);
         mainToolbar = findViewById(R.id.main_toolbar);
+        chargingImageView = findViewById(R.id.charging_imageview);
 
         // Retrieve saved out state, if it exists
         if (savedInstanceState != null) {
@@ -37,7 +61,7 @@ public class JobSchedulerActivity extends AppCompatActivity implements SharedPre
             counterTextView.setText(0 + "");
         }
 
-        /**
+        /*
          * Setup custom toolbar (used for launching navigation drawer)
          */
         setSupportActionBar(mainToolbar);
@@ -47,9 +71,62 @@ public class JobSchedulerActivity extends AppCompatActivity implements SharedPre
         actionbar.setHomeAsUpIndicator(R.drawable.ic_hamburger_menu);
         actionbar.setTitle("Job Scheduler");
 
-        /** Setup the shared preference listener **/
+        /* Setup the shared preference listener */
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
+
+        /* Intent filter for ChargingBroadcastReceiver */
+        chargingIntentFilter = new IntentFilter();
+        chargingIntentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        chargingIntentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        /* Registering the receiver */
+        chargingBroadcastReceiver = new ChargingBroadcastReceiver();
+    }
+
+    /**
+     * The activity has become visible (it is now "resumed").
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /** Determine the current charging state (because our dynamic BroadcastReceiver doesn't listen
+         * for changes when the app is closed) **/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            showCharging(batteryManager.isCharging());
+        } else {
+
+            // Create a new intent filter with the action ACTION_BATTERY_CHANGED. This is a
+            // sticky broadcast that contains a lot of information about the battery state.
+            IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            // Set a new Intent object equal to what is returned by registerReceiver, passing in null
+            // for the receiver. Pass in your intent filter as well. Passing in null means that you're
+            // getting the current state of a sticky broadcast - the intent returned will contain the
+            // battery information you need.
+            Intent currentBatteryStatusIntent = registerReceiver(null, ifilter);
+            // Get the integer extra BatteryManager.EXTRA_STATUS. Check if it matches
+            // BatteryManager.BATTERY_STATUS_CHARGING or BatteryManager.BATTERY_STATUS_FULL. This means
+            // the battery is currently charging.
+            int batteryStatus = currentBatteryStatusIntent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            boolean isCharging = batteryStatus == BatteryManager.BATTERY_STATUS_CHARGING ||
+                    batteryStatus == BatteryManager.BATTERY_STATUS_FULL;
+            // COMPLETED (8) Update the UI using your showCharging method
+            showCharging(isCharging);
+        }
+
+        /* Register the broadcast receiver that listens to changes in charging state */
+        registerReceiver(chargingBroadcastReceiver, chargingIntentFilter);
+    }
+
+    /**
+     * Another activity is taking focus (this activity is about to be "paused").
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(chargingBroadcastReceiver);
     }
 
     /**
@@ -80,9 +157,30 @@ public class JobSchedulerActivity extends AppCompatActivity implements SharedPre
         outState.putInt("currentCount", Integer.parseInt(counterTextView.getText().toString()));
     }
 
+    /**
+     * Broadcast receiver stuff
+     */
+    private class ChargingBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Boolean isCharging = action.equals(Intent.ACTION_POWER_CONNECTED);
+            showCharging(isCharging);
+        }
+    }
+
     /* Private helper methods */
     private void updateCounter() {
         int counterCount = PreferenceUtil.getCounterCount(this);
         counterTextView.setText(counterCount + "");
+    }
+
+    private void showCharging(Boolean isCharging) {
+        if (isCharging) {
+            chargingImageView.setVisibility(View.VISIBLE);
+        } else {
+            chargingImageView.setVisibility(View.INVISIBLE);
+        }
     }
 }
